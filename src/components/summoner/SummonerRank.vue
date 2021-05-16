18d13d30da4a68de253117de2bc8d361ecfcb6a2
JAE-GYU <shin3372@icloud.com>
2021-05-16 09:25:19 +0900

    feat: 랭크 스켈레톤 적용

--- src/components/summoner/SummonerRank.vue
+++ src/components/summoner/SummonerRank.vue
@@ -1,29 +1,37 @@
 <template>
-  <div class="summoner-rank" v-if="league.hasResults">
-    <img
-      class="summoner-rank-img"
-      src="https://opgg-static.akamaized.net/images/medals/platinum_1.png"
-      alt="rank-img"
-    />
-    <div class="summoner-rank-info">
-      <span class="rank-type">솔로 랭크</span>
-      <span class="most-position mt-4">탑 (총 27게임)</span>
-      <span class="tier mt-4">Platinum 1</span>
-      <span class="lp mt-6"><b>80 LP</b> / 28승 30패</span>
-      <span class="ratio mt-3">승률 51%</span>
+  <skeleton
+    tagName="div"
+    className="summoner-rank"
+    height="124px"
+    :class="{ unranked: !league.hasResults }"
+    :loading="false"
+  >
+    <div class="d-flex" v-if="league.hasResults">
+      <img
+        class="summoner-rank-img"
+        src="https://opgg-static.akamaized.net/images/medals/platinum_1.png"
+        alt="rank-img"
+      />
+      <div class="summoner-rank-info">
+        <span class="rank-type">솔로 랭크</span>
+        <span class="most-position mt-4">탑 (총 27게임)</span>
+        <span class="tier mt-4">Platinum 1</span>
+        <span class="lp mt-6"><b>80 LP</b> / 28승 30패</span>
+        <span class="ratio mt-3">승률 51%</span>
+      </div>
     </div>
-  </div>
-  <div class="summoner-rank unranked" v-else>
-    <img
-      class="summoner-rank-img"
-      src="@/assets/images/unranked.png"
-      alt="unrank-img"
-    />
-    <div class="summoner-rank-info">
-      <span class="rank-type">자유 5:5 랭크</span>
-      <span class="unranked mt-2">Unranked</span>
-    </div>
-  </div>
+    <template class="d-flex" v-else>
+      <img
+        class="summoner-rank-img"
+        src="@/assets/images/unranked.png"
+        alt="unrank-img"
+      />
+      <div class="summoner-rank-info">
+        <span class="rank-type">자유 5:5 랭크</span>
+        <span class="unranked mt-2">Unranked</span>
+      </div>
+    </template>
+  </skeleton>
 </template>
 
 <script>
