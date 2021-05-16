5b9a06f765101aabd96f646350a45136943f3ab5
JAE-GYU <shin3372@icloud.com>
2021-05-16 18:00:17 +0900

    fix:CSS 수정

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -18,5 +18,9 @@ export default {
 <style lang="scss" scoped>
 .layout-container {
   width: 100%;
+  height: 100%;
+  min-width: 1000px;
+  padding-bottom: 120px;
+  overflow-x: hidden;
 }
 </style>
